487e624516381855ae732ace3fb57fadfb697b8c
TUTULROY <tutulroyjoy@gmail.com>
2024-03-13 15:22:30 +0600

    explore core concepts

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+import Counter from './Counter';
+import Team from './Team';
+import Users from './User';
+import Friends from './Friends';
+
+function App() {
+  
+function handleClick(){
+  alert('button clicked');
+}
+
+const handleClick2 = () =>{
+  alert('button 2 clicked')
+}
+const addToFive = (num) =>{
+  alert(num + 5);
+}
+  return (
+    <>
+     
+      <h1>React Core Concepts 2</h1>
+      <Friends></Friends>
+
+      <Users></Users>
+
+<Team></Team>
+
+<Counter></Counter>
+
+
+     <button onClick={handleClick}>Click Me</button>
+     <button onClick={handleClick2}>Click ME2</button>
+     <button onClick={() => {alert('third click') }}>Third</button>
+<button onClick={() => addToFive(3)}>Four</button>
+     
+    </>
+  )
+}
+
+export default App
